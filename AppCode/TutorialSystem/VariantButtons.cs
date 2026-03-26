2f597efc7e561e4dec3b9cfc754f0071022c2f5d
iJungleboy <daniel.mettler@2sic.com>
2026-03-26 07:39:01 +0100

    protecting with page shield

--- AppCode/TutorialSystem/VariantButtons.cs
+++ AppCode/TutorialSystem/VariantButtons.cs
@@ -2,6 +2,7 @@ namespace AppCode.TutorialSystem
 {
   public class VariantButtons
   {
+    public string Id { get; set; }
     public string Label { get; set; }
     public string Classes { get; set; }
     public string Link { get; set; }
--- AppCode/TutorialSystem/VariantsService.cs
+++ AppCode/TutorialSystem/VariantsService.cs
@@ -10,45 +10,48 @@ namespace AppCode.TutorialSystem
     {
         var variants = Text.First(list, Variants.VariantsDefault);
 
-        var variantButtons = variants.Split(',').Select(variant => {
-          var variantIsSelected = variant == current;
-          var classes = variantIsSelected ? "btn btn-success" : "btn btn-primary";
-          var newVariant = variantIsSelected ? "" : variant;
-          var href = variantIsSelected ? "#" : Link.To(parameters: MyPage.Parameters.Set(Variants.VariantUrlParameter, newVariant));
-          var label = "";
-          switch (variant)
-          {
-            case Variants.VariantStrong:
-              if (variantIsSelected) {
-                label = "Selected: Strong-Typed (2sxc 17.06+)";
-              } else {
-                label = "Switch to Strong-Typed (2sxc 17.06+)";
+        var variantButtons = variants
+          .Split(',')
+          .Select(variant => 
+            {
+              var variantIsSelected = variant == current;
+              var classes = variantIsSelected ? "btn btn-success" : "btn btn-primary";
+              var newVariant = variantIsSelected ? "" : variant;
+              var href = variantIsSelected ? "#" : Link.To(parameters: MyPage.Parameters.Set(Variants.VariantUrlParameter, newVariant));
+              var label = "";
+              switch (variant)
+              {
+                case Variants.VariantStrong:
+                  if (variantIsSelected) {
+                    label = "Selected: Strong-Typed (2sxc 17.06+)";
+                  } else {
+                    label = "Switch to Strong-Typed (2sxc 17.06+)";
+                  }
+                  break;
+                case Variants.VariantTyped:
+                  if (variantIsSelected) {
+                    label = "Selected: Typed (2sxc 16+)";
+                  } else {
+                    label = "Switch to Typed (2sxc 16+)";
+                  }
+                  break;
+                case Variants.VariantDyn:
+                  if (variantIsSelected) {
+                    label = "Selected: Dynamic (Razor14 or below)";
+                  } else {
+                    label = "Switch to Dynamic (Razor14 or below)";
+                  }
+                  break;
+                case Variants.VariantPre12:
+                  if (variantIsSelected) {
+                    label = "Selected: Pre Razor12";
+                  } else {
+                    label = "Switch to Pre Razor12";
+                  }
+                  break;
               }
-              break;
-            case Variants.VariantTyped:
-              if (variantIsSelected) {
-                label = "Selected: Typed (2sxc 16+)";
-              } else {
-                label = "Switch to Typed (2sxc 16+)";
-              }
-              break;
-            case Variants.VariantDyn:
-              if (variantIsSelected) {
-                label = "Selected: Dynamic (Razor14 or below)";
-              } else {
-                label = "Switch to Dynamic (Razor14 or below)";
-              }
-              break;
-            case Variants.VariantPre12:
-              if (variantIsSelected) {
-                label = "Selected: Pre Razor12";
-              } else {
-                label = "Switch to Pre Razor12";
-              }
-              break;
-          }
-          return new VariantButtons { Classes = classes, Link = href, Label = label };
-        })
+              return new VariantButtons { Id = variant, Classes = classes, Link = href, Label = label };
+            })
         .ToList();
 
         return variantButtons;
